ed9d4708ee1dc5fe3c6163bc381b87bc98d00c47
elmerihyvonen <56589617+elmerihyvonen@users.noreply.github.com>
2020-01-19 23:56:32 +0200

    deployment to heroku

--- src/common/database.py
+++ src/common/database.py
@@ -9,7 +9,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['imageservice']
+        Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
