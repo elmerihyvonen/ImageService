78719625b20d67574c95dfbdd72b06599f2162e5
elmerihyvonen <56589617+elmerihyvonen@users.noreply.github.com>
2020-01-19 23:38:56 +0200

    deployment to heroku

--- src/common/database.py
+++ src/common/database.py
@@ -1,8 +1,9 @@
 import pymongo
+import os
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"  # default address and port for the database
+    URI = os.environ.get("MONGOLAB_URI")   # default address and port for the database
     DATABASE = None
 
     @staticmethod
